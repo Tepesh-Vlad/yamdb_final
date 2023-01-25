36eb9d6425c40306208d3080a049fcb1b4199faf
Tepesh-Vlad <shirocov.vladislav2015@yandex.ru>
2023-01-25 17:57:29 +0300

    test

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -7,6 +7,7 @@ class TestRequirements:
 
     def test_requirements(self):
         try:
+
             with open(f'{os.path.join(settings.BASE_DIR, "requirements.txt")}', 'r') as f:
                 requirements = f.read()
         except FileNotFoundError:
